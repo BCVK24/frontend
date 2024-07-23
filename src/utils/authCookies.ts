5fb4bbc17a7967b89bdc52c7bffd2abc7a1a6184
Dmitry Zaitsev <d.zaitsev@corp.mail.ru>
2024-07-24 00:14:47 +0300

    integration: added nginx + cookie -> LS

--- src/utils/authCookies.ts
+++ src/utils/authCookies.ts
@@ -1,4 +1,4 @@
-import Cookies from "js-cookie";
+// import Cookies from "js-cookie";
 
 export const GetAuthConfig = (contentType: string = "application/json") => {
   console.log(GetCookies());
@@ -11,26 +11,31 @@ export const GetAuthConfig = (contentType: string = "application/json") => {
 };
 
 export const GetCookies = () => {
-  const params = Cookies.get("fetchedParams");
+  // const params = Cookies.get("fetchedParams");
+  const params = localStorage.getItem("fetchedParams");
 
   if (!params) {
     UpdateCookies();
   }
 
-  return `VK ${Cookies.get("fetchedParams")}`;
+  return `VK ${localStorage.getItem("fetchedParams")}`;
+  // return `VK ${Cookies.get("fetchedParams")}`;
 };
 
 const UpdateCookies = () => {
   const HOUR = 3600;
-  const lastFetch = Cookies.get("lastFetch") || 0;
+  const lastFetch = +(localStorage.getItem("lastFetch") || 0);
   const now = new Date().getTime() / 1000;
 
   if (now - lastFetch < HOUR) return false;
 
-  Cookies.set("fetchedParams", btoa(window.location.search), {
-    expires: 1 / 24,
-  });
-  Cookies.set("lastFetch", now);
+  localStorage.setItem("fetchedParams", btoa(window.location.search))
+  localStorage.setItem("lastFetch", JSON.stringify(now))
+
+  // Cookies.set("fetchedParams", btoa(window.location.search), {
+  //   expires: 1 / 24,
+  // });
+  // Cookies.set("lastFetch", now);
 
   return true;
 };
